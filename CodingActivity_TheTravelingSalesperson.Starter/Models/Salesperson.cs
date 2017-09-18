3e4b6b7334ab730e2acffea83c48546d3a8a4ed6
Caitlin Conway <conway40@ad.nmc.edu>
2017-09-18 11:59:42 -0400

    Add project files.

---
+++ CodingActivity_TheTravelingSalesperson.Starter/Models/Salesperson.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingActivity_TheTravelingSalesperson
+{
+    public class Salesperson
+    {
+        #region FIELDS
+
+        private string _firstName;
+
+        private string _lastName;
+
+        private List<string> _citiesVisited;
+
+        private WidgetItemStock _item;
+
+
+        #endregion
+
+        #region PROPERTIES
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public List<string> CitiesVisited
+        {
+            get { return _citiesVisited; }
+            set { _citiesVisited = value; }
+        }
+
+        public WidgetItemStock Item
+        {
+            get { return _item; }
+            set { _item = value; }
+        }
+
+        public WidgetItemStock Item1
+        {
+            get { return _item; }
+            set { _item = value; }
+        }
+
+        #endregion
+
+
+        #region CONSTRUCTORS
+
+        public Salesperson()
+        {
+            _citiesVisited = new List<string>();
+            _item = new WidgetItemStock();
+        }
+
+        public Salesperson(string firstName/*, string lastName*/)
+        {
+            _firstName = firstName;
+            //_lastName = lastName;
+            _citiesVisited = new List<string>();
+            _item = new WidgetItemStock();
+        }
+        
+
+        #endregion
+
+
+        #region METHODS
+
+
+
+        #endregion
+    }
+}
